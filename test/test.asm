dff4ce567bc11cfa9b2500c738d90269b672b216
Emil Taylor Bye <emil@wodinaz.com>
2013-10-08 19:00:54 +0200

    Added labels to assembly

--- test/test.asm
+++ test/test.asm
@@ -6,11 +6,14 @@ LW $1,0x1($0) ; $1=Fn-2
 LW $2,0x2($0) ; $2=Fn-1
 LW $3,0xf($0) ; $3=Number limit
 
+LOOP:
 ADD $4,$1,$2
 ADD $1,$2,$0 ; Move $2 to $1
 ADD $2,$4,$0 ; Move $4 to $2
 SLT $4,$3,$2 ; $4 = $3 < $2
-BEQ $0,$4,-0x4 ; If $3 > $2, go to 0x4
+BEQ $0,$4,LOOP ; If $3 > $2, go to 0x4
 
 SW $3,0x10($0) ; Store value at 0x10
-J 0x
+
+INFILOOP:
+J INFILOOP
